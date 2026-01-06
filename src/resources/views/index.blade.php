9f3bec63cdf1acd5a2e24267461e15457b5ba7ef
root <root@S09-0813-pc.ohizumi-mfg.corp>
2026-01-06 16:51:27 +0900

    Todo初級編完了

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="todo__alert">
+    <div class="todo__alert--success">
+        {{session('message')}}
+    </div>
+</div>
+<!-- Content本体 -->
+<div class="todo__content">
+    <!-- Create Form -->
+    <form class="create-form" action="/todos" method="post">
+        @csrf
+        <div class="create-form__item">
+            <input class="create-form__item-input" type="text" name="content">
+        </div>
+        <div class="create-form__button">
+            <button class="create-form__button-submit" type="submit">作成</button>
+        </div>
+    </form>
+    <!-- Todo List -->
+    <div class="todo-table">
+        <table class="todo-table__inner">
+            <!-- LINE1 -->
+            <tr class="todo-table__row">
+                <th class="todo-table__header">Todo</th>
+            </tr>
+            @foreach($todos as $todo)
+            <!-- LINE2 -->
+            <tr class="todo-table__row">
+                <td class="todo-table__item">
+                    <form class="update-form">
+                        <div class="update-form__item">
+                            <input class="update-form__item-input">{{ $todo['content'] }}</input>
+                        </div>
+                        <div class="update-form__button">
+                            <button class="update-form__button-submit" type="submit">更新</button>
+                        </div>
+                    </form>
+                </td>
+                <td class="todo-table__item">
+                    <form class="delete-form">
+                        <div class="delete-form__button">
+                            <button class="delete-form__button-submit" type="submit">削除</button>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+use App\Models\Todo;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', [TodoController::class,'index']);
+Route::post('/todos', [TodoController::class,'store']);
+Route::get('/',[TodoController::class,'indexTest']);
+Route::patch('/todos/update',[TodoController::class,'update']);
+Route::delete('/todos/delete',[TodoController::class,'destroy']);
